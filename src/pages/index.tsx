b897732b4700c8f3c3f111c7000457c0ddeeae94
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-05 20:26:08 +0100

    Added Jest, ESLinter, Pre-commits and upgraded all dependencies.

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import {Main} from "../layout";
+
+const Title = styled.h1`
+  color: ${({theme}) => theme.colors.primary};
+  font-size: 50px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+`;
+
+Title.displayName = "Title";
+
+const Index: React.FunctionComponent = () => {
+    return (
+        <Main>
+            <Title>
+                Hello World!
+            </Title>
+        </Main>
+    );
+};
+
+export default Index;
---
+++ src/settings/theme.ts
@@ -0,0 +1,6 @@
+export default {
+    colors: {
+        primary: "#0f0fa0",
+        secondary: "#9d9e9f"
+    }
+};
