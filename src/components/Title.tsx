539721cfbe3289b2b225f4f455221c6e0d3b63ad
Amadeusz Blanik <amadeusz@blanik.me>
2020-12-06 21:43:23 +0100

    Added server side styled components

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+
+  a {
+    color: #0070f3;
+    text-decoration: none;
+
+    &:hover,
+    &:focus,
+    &:active {
+      text-decoration: underline;
+    }
+  }
+`;
 
 const Title: React.FC = () => (
   <>
-    <h1 className="title">
+    <StyledTitle>
       Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </h1>
-    <style jsx>
-      {`
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-          text-align: center;
-        }
-      `}
-    </style>
+    </StyledTitle>
   </>
 );
 
